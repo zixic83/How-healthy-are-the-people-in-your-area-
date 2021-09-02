e0e7384de84e1db1f44bdb13009a7535185077f9
Alice Chen <zixic@student.unimelb.edu.au>
2021-09-02 21:09:30 +1000

    Rendered bar charts

--- health_risk_factors/src/App.js
+++ health_risk_factors/src/App.js
@@ -1,8 +1,14 @@
+import BarChart from "./BarChart";
+import daily_smoker_data from "../src/data/daily_smoker.json";
+import over_two_drinks_data from "../src/data/consume_over_two_drinks.json";
+import physcial_activity_data from "../src/data/insufficent_physical_activity.json";
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <BarChart rawData={daily_smoker_data} topic='Daily smoker'></BarChart>
+      <BarChart rawData={over_two_drinks_data} topic='Alcohol comsumption'></BarChart>
+      <BarChart rawData={physcial_activity_data } topic='Physical activity'></BarChart>
     </div>
   );
 }
